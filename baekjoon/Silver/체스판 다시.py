5a56022fc43fb1da3d246c4c75c0d0447e701d5d
henryeffel <alfzm102435@gmail.com>
2025-08-06 00:53:49 +0900

    넘 어려받ㅇ

--- baekjoon/Silver/체스판 다시.py
+++ baekjoon/Silver/체스판 다시.py
@@ -1,2 +1,29 @@
-#체스판을 다시 칠해야 한다, 검은색과 흰색이 칠해져야한다
-#인접한 두곳은 다른색이여햐한다
+n,m = map(int(input().split()))
+#보드 데이터 입력받기
+board = []
+b,w = input().split()
+for _ in range(n):
+    board.append(input().split())
+board = [input().strip() for _ in range(n)]
+
+min_count = 64
+
+for row in range(n-7):
+    for col in range(m-7):
+        count1 = 0 #0,0이 w
+        coutn2 = 0 #0,0이 b
+        for i in range(8):
+            for j in range(8):
+                current = board[row + i][col + j]
+                if (i+j)%2 == 0:
+                    if current != "w":
+                        count1 += 1
+                    if current != "b":
+                        count2 += 1
+                else :
+                    if current != "b":
+                        count1 += 1
+                    if current != "w":
+                        count2 += 1
+min_count = min(min_count,count1,count2)
+print(min_count)
